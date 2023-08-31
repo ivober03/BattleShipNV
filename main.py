a9d999e4592e5c1f9f4dbf612736abad5b9ac39f
Ivo Bermudez De Marchi <125988184+ivober03@users.noreply.github.com>
2023-08-31 22:27:51 +0000

    main v0.1

--- main.py
+++ main.py
@@ -1,37 +1,75 @@
+import random
 
+class Cell:
 
-print ("hello")
+    def __init__(self, row, col):
+        """
+        Constructor for the Cell class.
+        Initializes attributes such as row and column.
+        """
+        self.row = row
+        self.col = col
+        self.pdf_value = 0
+
 
 class Ship:
 
-    """"Class representing a ship of difrent size and position"""
-
     def __init__(self, size):
         """
-        Include:
-        Health
-        cells: set of coordenates of the boat in the board
+        Constructor for the Ship class.
+        Initializes attributes such as health and cells.
         """
-        raise NotImplementedError
+        self.health = size  # Initialize health based on ship size
+        self.cells = []  # Keep track of occupied cells on the board
 
-    def place_on_board(self, cells ):
+    def place_on_board(self, cells):
         """
-        Places the ship on the board, and stores the cells ocupied by the boat
+        Places the ship on the board and updates the occupied cells list.
         """
-        raise NotImplementedError
-    
+        self.cells = cells
+
     def is_alive(self):
         """
-        Returns true if the boat haves at least 1 of health
+        Checks if the ship is still afloat (health > 0).
+        Returns True if the ship is alive, False otherwise.
         """
-        raise NotImplementedError
-    
+        return self.health > 0
+
     def hit(self):
         """
-        Decrements the health by 1
+        Handles a hit on the ship, decreasing health by 1.
         """
-        raise NotImplementedError
-    
+        self.health -= 1
 
 
+class Sentence:
+    """
+    Logical statement about a Battleship game.
+    A sentence consists of a set of board cells which are possible targets.
+    """
 
+    def __init__(self, ship, cells):
+        """
+        Constructor for the Sentence class.
+        Initializes the ship and its corresponding cells.
+        """
+
+    def __eq__(self, other):
+        """
+        Compare sentences.
+        """
+
+    def __str__(self):
+        """
+        Sentence as a string.
+        """
+
+    def add_cells(self, cells):
+        """
+        Add cells to the sentence.
+        """
+
+    def remove_cells(self, cells):
+        """
+        Remove cells from the sentence.
+        """
