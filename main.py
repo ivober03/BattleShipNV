455e9b33e2f051e3046e45bf454a72196b82f644
Ivo Bermudez De Marchi <125988184+ivober03@users.noreply.github.com>
2023-09-01 13:56:46 +0000

    main v0.03

--- main.py
+++ main.py
@@ -65,7 +65,7 @@ class Sentence:
 
     def __str__(self):
         """
-        Sentence as a string.
+        Returns the sentence as a string.
         """
 
     def add_cells(self, cells):
@@ -86,34 +86,30 @@ class Battleship:
 
     def __init__(self, width=10, heigth=10):
         """
-        Implement:
-        User:create a instance of user class
-        OponnentIA: create a instance of OponentIA class
-        
-        
+        Constructor for the battleship class.
+        Initializes the board, user, and opponent.
         """
         self.width = width
         self.heigth = heigth
-        self.board = [] #remember to intialize the matrix board
+        self.board = [] # remember to intialize the matrix board
         self.turn = "user"
         
         
     def won(self):
         """
-        Indicates if one of the players haves sunked all the enemy boats
+        Returns true if one of the players haves sunked all the enemy boats
         """
 
 
     def check_hit(self, cell):
         """
-        Indicates if the guess at the specified cell is a hit or a miss
+        Returns true if the shot at the specified cell is a hit, otherwise returns false
         """
-        self.cell = cell
 
 
     def switch_turn(self):
         """
-        Switches the turn beetwen user and opponent
+        Switches the turn beetwen the players
         """
         if self.turn == "user":
             self.turn = "oponnent"
@@ -123,28 +119,23 @@ class Battleship:
 
 class OpponentAi:
     """
-    Represents the AI opponent int he game
+    Represents the AI opponent
     """
-    def __init__(self, width, heigth):
 
+    def __init__(self, width, heigth):
         """
-        Implement:
-        
-        Self board
-        Oponent board
-        
+        Constructor for the AI opponent.
         """
         self.width = width
         self.heigth = heigth
-        self.pdf_matrix = [] #Represents the board where the IA makes his guesses
+        self.pdf_matrix = [] # 
         self.moves_made = set()
         self.mode = "hunt"
 
 
-
     def place_boats_random(self):
         """
-        places the boats randomly
+        Places AI boats randomly
         """
 
 
@@ -156,32 +147,36 @@ class OpponentAi:
 
     def make_guess(self):
         """
-        Generates a guess based on sentences and the pdf
+        Makes a guess based on sentences and the pdf value for each cell
         """
 
 
     def hunt(self):
         """
-        In Hunt mode, fire at random coordinates with even parity to increase chances of hitting ships.
+        In Hunt mode, fire at random coordinates with even parity.
         """
 
 
     def target(self):
         """
-        In this mode:
-        1-Creates a sentence with 4 possible cells to continue hitting the boat
-        2-Select from that 4 cells who has the highest euristic value and fires(Iterative till found another part of the ship or sunked)
-        3-Creates a new sentence taking into account the orientation and all of the knowledge
-        4-Fires at the most probable cell from the sentence 
-        5-Iterate 3 and 4 till boat sunked
+        Enter target mode once a boat is hit, then:
+            1-Create a sentence with 4 possible cells to continue hitting the boat.
+            2-Select from that 4 cells who has the highest euristic value and fire.
+            3-Create a new sentence taking into account the orientation and all available knowledge.
+            4-Fire at the most probable cell from the sentence.
+            5-Iterate steps 3 and 4 till the boat is sunked or no new sentence can be generated.
         """
 
     
-    def update_sentence(self):
+    def update_knowledge(self):
         """
-        Updates the sentence based on the guess result
+        Updates the sentence based on the guess result(hit or miss)
         """
 
+    def generate_sentence(self):
+        """
+        Generate a new sentence once a target is hitn
+        """
 
 class User:
     """
