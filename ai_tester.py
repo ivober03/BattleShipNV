b8b73adb99bb9ddde067da9b33504d6a053d4904
Ivo Bermudez De Marchi <125988184+ivober03@users.noreply.github.com>
2023-10-23 08:42:17 -0300

    Update ai_tester.py

--- ai_tester.py
+++ ai_tester.py
@@ -7,24 +7,27 @@ from battleship.constants import *
 
 WIN = pygame.display.set_mode((2*WIDTH + SEPARATION, HEIGHT)) 
 pygame.display.set_caption('Battleship')
+
 def main():
+
     Run = True
     clock = pygame.time.Clock() # Sets the fps to 60
+
+    # Create an instance of Board and User to test the AI
     opponent_board = Board(1) 
     opponent = User(opponent_board)
     ai = OpponentAI(None, opponent)
+    
     while Run:
         clock.tick(60)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit();
                     
-        # Create an instance of Board and User to test the AI
-
         opponent.put_ship(((1, 3), (2, 3), (3, 3), (4, 3), (5, 3)), WIN)
         opponent.put_ship(((4, 7), (5, 7), (6, 7), (7, 7), (8, 7)), WIN)
         make_guess_test(ai)
-        pygame.display.update()
+
     pygame.quit()
 
 
@@ -32,18 +35,20 @@ def make_guess_test(ai):
     """
     Tester for make guess method
     """
+
     knowledge_set = ai.knowledge
+
     # Simulate a situation where the AI has no knowledge
     if not knowledge_set:
         print("Testing 'make_guess' with no knowledge:")
         is_hit, cell = ai.make_guess()
         print(f"AI's guess: {cell}, Hit: {is_hit}")
         print(f"Knowledge: {knowledge_set}")
+
+    # Simulate a situation where the AI has no knowledge
     else: 
         print("Testing 'make_guess' with knowledge:")
         is_hit, cell = ai.make_guess()
         print(f"AI's guess: {cell}, Hit: {is_hit}")
         print(f"Knowledge: {knowledge_set}")
 
-
-main()
